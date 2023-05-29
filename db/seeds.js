45c655aa83507ce8f43fa3ce83faedb21b95e501
a-wong-8 <awong88@berkeley.edu>
2023-05-28 20:26:09 -0700

    working on side pics

--- db/seeds.js
+++ db/seeds.js
@@ -1,14 +1,14 @@
 const img1 = "1"
 
 const car1 = {makemodel: '2004 Honda S2000', engine: '240 hp L4 2.2L', weight: '2835 lbs', time: 14.4, price: '$32,800', img: './db/smalls2000.png', bigimg: './db/s2000-2.png'};
-const car2 = {makemodel: '1991 Mazda Miata MX-5', engine: '116 hp l4 1.6L', weight: '2210 lbs', time: 16.78, price: '$15,869', img: './db/smallmiata.png'};
-const car3 = {makemodel: '2009 Ferrari F430 Scuderia', engine: '503 hp v8 4.3L', weight: '2750 lbs', time: 11.4, price: '$281,000', img: './db/smallscud.png'};
-const car4 = {makemodel: '2006 Mitsubishi Lancer Evo 9 MR', engine: '286 hp l4 2.0L', weight: '3200 lbs', time: 13.4, price: '$35,000', img: './db/smallevo.png'};
-const car5 = {makemodel: '2015 Ferrari 458 Italia', engine: '570 hp v8 4.5L', weight: '3274 lbs', time: 10.8, price: '$245,000', img: './db/small458.png'};
-const car6 = {makemodel: '2019 Lamborghini Huracan Performante', engine: '630 hp v10 5.2L', weight: '3047 lbs', time: 10.1, price: '$209,000', img: './db/smalllamb.png'};
-const car7 = {makemodel: '2021 Tesla Model S Plaid', engine: '1020 hp electric', weight: '4766 lbs', time: 9.34, price: '$130,000', img: './db/smalltesla.png'};
-const car8 = {makemodel: '1908 Ford Model T', engine: '20 hp l4 2.9L', weight: '1,650 lbs', time: 32, price: '$850', img: './db/smallt.png'};
-const car9 = {makemodel: '2015 Toyota Prius', engine: '98 hp l4 1.8l hybrid', weight: '3072 lbs', time: 17.9, price: '$30,000', img: './db/smallprius.png'};
-const car10 = {makemodel: '2006 Chevrolet Corvette Z06', engine: '505 hp v8 7.0l', weight: '3130 lbs', time: 11.5, price: '$65,800', img: './db/smallz06.png'};
+const car2 = {makemodel: '1991 Mazda Miata MX-5', engine: '116 hp l4 1.6L', weight: '2210 lbs', time: 16.78, price: '$15,869', img: './db/smallmiata.png', bigimg: './db/miata.png'};
+const car3 = {makemodel: '2009 Ferrari F430 Scuderia', engine: '503 hp v8 4.3L', weight: '2750 lbs', time: 11.4, price: '$281,000', img: './db/smallscud.png', bigimg: './db/scud.png'};
+const car4 = {makemodel: '2006 Mitsubishi Lancer Evo 9 MR', engine: '286 hp l4 2.0L', weight: '3200 lbs', time: 13.4, price: '$35,000', img: './db/smallevo.png', bigimg: './db/evo.png'};
+const car5 = {makemodel: '2015 Ferrari 458 Italia', engine: '570 hp v8 4.5L', weight: '3274 lbs', time: 10.8, price: '$245,000', img: './db/small458.png', bigimg: './db/458.png'};
+const car6 = {makemodel: '2019 Lamborghini Huracan Performante', engine: '630 hp v10 5.2L', weight: '3047 lbs', time: 10.1, price: '$209,000', img: './db/smalllamb.png', bigimg: './db/lamb.png'};
+const car7 = {makemodel: '2021 Tesla Model S Plaid', engine: '1020 hp electric', weight: '4766 lbs', time: 9.34, price: '$130,000', img: './db/smalltesla.png', bigimg: './db/tesla.png'};
+const car8 = {makemodel: '1908 Ford Model T', engine: '20 hp l4 2.9L', weight: '1,650 lbs', time: 32, price: '$850', img: './db/smallt.png', bigimg: './db/modelt.png'};
+const car9 = {makemodel: '2015 Toyota Prius', engine: '98 hp l4 1.8l hybrid', weight: '3072 lbs', time: 17.9, price: '$30,000', img: './db/smallprius.png', bigimg: './db/prius.png'};
+const car10 = {makemodel: '2006 Chevrolet Corvette Z06', engine: '505 hp v8 7.0l', weight: '3130 lbs', time: 11.5, price: '$65,800', img: './db/smallz06.png', bigimg: './db/z06.png'};
 
 module.exports = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10];
