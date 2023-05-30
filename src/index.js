1f1f876a9e3ceab77b56dd433a8af84c4bcb6556
a-wong-8 <awong88@berkeley.edu>
2023-05-30 13:43:34 -0700

    reset doneish

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let currentSide = 'left'; // sets default side to left 
 
-    initToggle('toggleInput', toggleCallback); 
-    // takes in the toggle switch position then sets current side to left or right 
+    initToggle('toggleInput', toggleCallback); // takes in the toggle switch position then sets current side to left or right 
 
     setupSelectionOfCars(selectionCallback); // creates the ul, takes in callback and will pass the selected car to callback 
 
@@ -22,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let rightCarTime = 0;
 
     function selectionCallback (car) {
-
+        
         if (currentSide === 'left') {
             leftSelected = true;
             leftCarTime = car.time;
@@ -40,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const divElementRightCar = document.querySelector('.right-car');    
             divElementRightCar.innerHTML = `<ul><li>Car: ${car.makemodel}</li> <li>Engine: ${car.engine}</li> <li>Weight: ${car.weight}</li> <li>Quarter mile time (sec): ${car.time}</li> <li>MSRP: ${car.price}</li></ul>`;
 
-            const divElementRightCarPic = document.querySelector('.right-car-pic'); // makes the left pic
+            const divElementRightCarPic = document.querySelector('.right-car-pic'); // makes the right pic
             divElementRightCarPic.innerHTML = `<img src=${car.bigimg}></img>`;
         }
 
@@ -48,7 +47,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
             let car1 = document.querySelector("#left-drag-car");
             let car2 = document.querySelector("#right-drag-car");
-            
+            // window.car1 = car1;
+            // window.car2 = car2;
+
             let distance = -480; // Distance to the finish line
             let car1Position = 1;
             let car2Position = 1;
@@ -60,9 +61,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 car2Position = (leftCarTime / rightCarTime) - 1;
                 car1Position = 0;
             }
-            
-            // car1.style.top = car1Position * distance + "px";
-            // car2.style.top = car2Position * distance + "px";
 
             let car1top = car1Position * distance + "px";
             let car2top = car2Position * distance + "px";
@@ -78,9 +76,17 @@ document.addEventListener("DOMContentLoaded", () => {
             const keyframesRight = styleSheet.cssRules[1];
             keyframesRight.deleteRule(0);
             keyframesRight.appendRule(`to { top: ${car2top}; }`);
+        }
 
-            car1.classList.add('animate'); // adds animate after both cars selected 
-            car2.classList.add('animate');
-            }
+        setTimeout(resetCars, 10000);
+    
+        function resetCars () {
+            let car1 = document.querySelector("#left-drag-car");
+            let car2 = document.querySelector("#right-drag-car");
+            car1.style.animation = 'none';
+            car2.style.animation = 'none';
+            car1.style.top = '465px';
+            car2.style.top = '465px';
+        }
     }      
   });
