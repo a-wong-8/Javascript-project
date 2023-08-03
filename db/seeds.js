26ddac3d25846fa3492704e8646483ed8eb44e05
a-wong-8 <awong88@berkeley.edu>
2023-08-02 20:42:45 -0700

    revised stats

--- db/seeds.js
+++ db/seeds.js
@@ -1,23 +1,23 @@
 // const img1 = "1"
 
-const car1 = {makeModel: '2004 Honda S2000', engine: '240 hp L4 2.2L', weight: '2835 lbs', time: 14.4, price: '$32,800', img: './db/smalls2000.png', bigimg: './db/s2000-2.png', mpg: '20 city / 25 hwy', speed: 6, priceRating: 3, mpgRating: 5};
+const car1 = {makeModel: '2004 Honda S2000', engine: '240 hp L4 2.2L', weight: '2,835 lbs', time: 14.4, price: '$32,800', img: './db/smalls2000.png', bigimg: './db/s2000-2.png', mpg: '20 city / 25 hwy', speed: 6, priceRating: 3, mpgRating: 5};
 
-const car2 = {makeModel: '1991 Mazda Miata MX-5', engine: '116 hp L4 1.6L', weight: '2210 lbs', time: 16.78, price: '$15,869', img: './db/smallmiata.png', bigimg: './db/miata.png', mpg: '22 city / 28 hwy', speed: 4, priceRating: 2, mpgRating: 7};
+const car2 = {makeModel: '1991 Mazda Miata MX-5', engine: '116 hp L4 1.6L', weight: '2,210 lbs', time: 16.78, price: '$15,869', img: './db/smallmiata.png', bigimg: './db/miata.png', mpg: '22 city / 28 hwy', speed: 4, priceRating: 2, mpgRating: 7};
 
-const car3 = {makeModel: '2009 Ferrari F430 Scuderia', engine: '503 hp V8 4.3L', weight: '2750 lbs', time: 11.4, price: '$281,000', img: './db/smallscud.png', bigimg: './db/scud.png', mpg: '11 city / 16 hwy', speed: 8, priceRating: 10, mpgRating: 2};
+const car3 = {makeModel: '2009 Ferrari F430 Scuderia', engine: '503 hp V8 4.3L', weight: '2,750 lbs', time: 11.4, price: '$281,000', img: './db/smallscud.png', bigimg: './db/scud.png', mpg: '11 city / 16 hwy', speed: 8, priceRating: 10, mpgRating: 2};
 
-const car4 = {makeModel: '2006 Mitsubishi Lancer Evo 9 MR', engine: '286 hp L4 2.0L', weight: '3200 lbs', time: 13.4, price: '$35,000', img: './db/smallevo.png', bigimg: './db/evo.png', mpg: '19 city / 25 hwy', speed: 7, priceRating: 4, mpgRating: 5};
+const car4 = {makeModel: '2006 Mitsubishi Lancer Evo 9 MR', engine: '286 hp L4 2.0L', weight: '3,200 lbs', time: 13.4, price: '$35,000', img: './db/smallevo.png', bigimg: './db/evo.png', mpg: '19 city / 25 hwy', speed: 7, priceRating: 4, mpgRating: 5};
 
-const car5 = {makeModel: '2015 Ferrari 458 Italia', engine: '570 hp V8 4.5L', weight: '3274 lbs', time: 10.8, price: '$245,000', img: './db/small458.png', bigimg: './db/458.png', mpg: '13 city / 17 hwy', speed: 9, priceRating: 10, mpgRating: 2};
+const car5 = {makeModel: '2015 Ferrari 458 Italia', engine: '570 hp V8 4.5L', weight: '3,274 lbs', time: 10.8, price: '$245,000', img: './db/small458.png', bigimg: './db/458.png', mpg: '13 city / 17 hwy', speed: 9, priceRating: 10, mpgRating: 2};
 
-const car6 = {makeModel: '2019 Lamborghini Huracan Performante', engine: '630 hp V10 5.2L', weight: '3047 lbs', time: 10.1, price: '$209,000', img: './db/smalllamb.png', bigimg: './db/lamb.png', mpg: '13 city / 18 hwy', speed: 9, priceRating: 10, mpgRating: 2};
+const car6 = {makeModel: '2019 Lamborghini Huracan Performante', engine: '630 hp V10 5.2L', weight: '3,047 lbs', time: 10.1, price: '$209,000', img: './db/smalllamb.png', bigimg: './db/lamb.png', mpg: '13 city / 18 hwy', speed: 9, priceRating: 10, mpgRating: 2};
 
-const car7 = {makeModel: '2021 Tesla Model S Plaid', engine: '1020 hp Electric', weight: '4766 lbs', time: 9.34, price: '$130,000', img: './db/smalltesla.png', bigimg: './db/tesla.png', mpg: '102 city / 99 hwy', speed: 10, priceRating: 8, mpgRating: 10};
+const car7 = {makeModel: '2021 Tesla Model S Plaid', engine: '1020 hp Electric', weight: '4,766 lbs', time: 9.34, price: '$130,000', img: './db/smalltesla.png', bigimg: './db/tesla.png', mpg: '102 city / 99 hwy', speed: 10, priceRating: 8, mpgRating: 10};
 
 const car8 = {makeModel: '2015 Nissan GT-R Premium', engine: '545 hp V6 3.8L', weight: '2,827 lbs', time: 11.2, price: '$101,770', img: './db/smallgtr.png', bigimg: './db/gtr.png', mpg: '16 city / 23 hwy', speed: 8, priceRating: 7, mpgRating: 5};
 
-const car9 = {makeModel: '2015 Toyota Prius', engine: '98 hp L4 1.8L Hybrid', weight: '3072 lbs', time: 17.9, price: '$30,000', img: './db/smallprius.png', bigimg: './db/prius.png', mpg: '53 city / 46 hwy', speed: 2, priceRating: 3, mpgRating: 9};
+const car9 = {makeModel: '2015 Toyota Prius', engine: '98 hp L4 1.8L Hybrid', weight: '3,072 lbs', time: 17.9, price: '$30,000', img: './db/smallprius.png', bigimg: './db/prius.png', mpg: '53 city / 46 hwy', speed: 2, priceRating: 3, mpgRating: 9};
 
-const car10 = {makeModel: '2006 Chevrolet Corvette Z06', engine: '505 hp V8 7.0L', weight: '3130 lbs', time: 11.5, price: '$65,800', img: './db/smallz06.png', bigimg: './db/z06.png', mpg: '18 city / 28 hwy', speed: 8, priceRating: 6, mpgRating: 5};
+const car10 = {makeModel: '2006 Chevrolet Corvette Z06', engine: '505 hp V8 7.0L', weight: '3,130 lbs', time: 11.5, price: '$65,800', img: './db/smallz06.png', bigimg: './db/z06.png', mpg: '18 city / 28 hwy', speed: 8, priceRating: 6, mpgRating: 5};
 
 module.exports = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10];
